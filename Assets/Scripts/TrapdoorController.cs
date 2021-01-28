ae76f49f4961cbb448aa8732332e7f7e7001d4f5
montypyle <77437020+montypyle@users.noreply.github.com>
2021-01-28 10:58:50 +0000

    edit

---
+++ Assets/Scripts/TrapdoorController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(HingeJoint2D))]
+
+public class TrapdoorController : MonoBehaviour
+{
+        private HingeJoint2D trapdoorHinge;
+
+        private void Awake()
+        {
+            // Initialise the hinge variable (saves me having to "wire it
+            // up" via the inspector)
+            trapdoorHinge = GetComponent<HingeJoint2D>();
+        }
+
+
+        public void SetTrapdoorSpeed(float speedToSet)
+        {
+            JointMotor2D trapdoorMotor = trapdoorHinge.motor;
+            trapdoorMotor.motorSpeed = speedToSet;
+            trapdoorHinge.motor = trapdoorMotor;
+        }
+    }
